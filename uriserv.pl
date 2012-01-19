a98ed1ef6effabf5c35ba6a29e517561411f7fad
Daniel Mussell <shadertest@shadertest.ca>
2012-01-18 23:19:29 -0500

    modify uriserv to use WWW::Curl

--- uriserv.pl
+++ uriserv.pl
@@ -9,18 +9,12 @@ use POSIX qw(setsid);
 use IO::Socket::UNIX;
 use Encode;
 
-use LWP::UserAgent;
 use HTML::HeadParser;
-use LWP::Protocol::https;
+use WWW::Curl::Easy;
 
 my %options = do "rc.pl";
 die "\e[31m[FAIL] Could not parse rc.pl: $@\n" if ($@);
 die "\e[31m[FAIL] Could not open rc.pl: $!\n" unless (%options);
-
-my $ua =
-  new LWP::UserAgent( agent =>
-      "Mozilla/5.0 (X11; Linux x86_64; rv:11.0a1) Gecko/20111111 Firefox/11.0a1"
-  );
 
 my $socket = new IO::Socket::UNIX(
     Type => SOCK_STREAM,
@@ -42,25 +36,43 @@ sub convertBytes {
 
 sub getTitle {
     my $uri = shift;
-    $uri =~ s/(\x03(?:\d{1,2}(?:,\d{1,2})?)?|\x02|\x1f|\x0f|x16)//g;
-    my $response = $ua->head($uri);
-    my $type     = $response->header('Content-Type');
-    my $length   = $response->header('Content-Length');
+    $uri =~ s/(\x03(?:\d{1,2}(?:,\d{1,2})?)?|\x02|\x1f|\x0f|\x16)//g;
+    my $curl = new WWW::Curl::Easy;
 
-    if ( $type =~ m/text\/html/ ) {
-        return "\0034ERROR: File too large\003" if ( $length > 6291456 );
-        my $p = HTML::HeadParser->new;
-        $ua->max_size(10240);
-        $response = $ua->get($uri);
-        $p->parse( $response->decoded_content );
-        my $title = $p->header('Title');
-        $title = $response->code && next unless ($title);
+    $curl->setopt( CURLOPT_USERAGENT,
+"Mozilla/5.0 (X11; Linux x86_64; rv:12.0a1) Gecko/20120117 Firefox/12.0a1"
+    );
+    $curl->setopt( CURLOPT_NOBODY,         1 );   # We want to do a HEAD request
+    $curl->setopt( CURLOPT_FOLLOWLOCATION, 1 );   # follow redirects
+    $curl->setopt( CURLOPT_MAXREDIRS,      6 );   # but only 6 of them
+    $curl->setopt( CURLOPT_URL,            $uri );    # pass the actual URI
+    $curl->setopt( CURLOPT_SSL_VERIFYPEER, 0 );       # allow self-signed,
+    $curl->setopt( CURLOPT_SSL_VERIFYHOST, 0 );       # expired, bad certs
+    my $retcode = $curl->perform;
+    return if ( $retcode == 6 );
+
+    return "curl returned $retcode (" . $curl->strerror($retcode) . ")"
+      unless ( $retcode == 0 or $retcode == 6 );
+    my $type = $curl->getinfo(CURLINFO_CONTENT_TYPE);
+    my $size = $curl->getinfo(CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+
+    if ( $type =~ m/text\/html/ and $size < 6291456 ) {
+        my $body;
+        $curl->setopt( CURLOPT_HTTPGET,   1 );        # now we GET
+        $curl->setopt( CURLOPT_WRITEDATA, \$body );
+        $retcode = $curl->perform;
+        return "curl returned $retcode (" . $curl->strerror($retcode) . ")"
+          unless ( $retcode == 0 or $retcode == 6 );
+        my $parser = new HTML::HeadParser;
+        $parser->parse($body);
+        my $title = $parser->header('Title');
         $title =~ s/\R/ /gmi;
         return "[\0033URI\003] $title";
     }
-    return undef unless ($length);
-    $length = convertBytes($length);
-    return "[\0033URI\003] \'$type\' $length";
+
+    # return undef unless ($length);
+    $size = convertBytes($size);
+    return "[\0033URI\003] \'$type\' $size";
 }
 
 sub getTitles {
