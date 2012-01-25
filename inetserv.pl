f5f087e49010575ad2482b875d24db2120a02f6f
Daniel Mussell <shadertest@shadertest.ca>
2012-01-25 01:24:51 -0500

    port inetserv to WWW::Curl

--- inetserv.pl
+++ inetserv.pl
@@ -8,18 +8,15 @@ use diagnostics;
 use POSIX qw(setsid);
 use IO::Socket::UNIX;
 
-use LWP;
 use XML::Simple;
 use URI::Escape;
 use JSON;
 use HTML::Entities;
-use LWP::Protocol::https;
+use WWW::Curl::Easy;
 
 my %options = do "rc.pl";
 die "\e[31m[FAIL] Could not parse rc.pl: $@\n" if ($@);
 die "\e[31m[FAIL] Could not open rc.pl: $!\n" unless (%options);
-
-my $ua = LWP::UserAgent->new();
 
 my $socket = new IO::Socket::UNIX(
     Type => SOCK_STREAM,
@@ -28,9 +25,14 @@ my $socket = new IO::Socket::UNIX(
 
 sub metar {
     my $icao     = shift;
-    my $response = $ua->get(
-        "ftp://tgftp.nws.noaa.gov/data/observations/metar/stations/$icao.TXT");
-    my $data = $response->decoded_content;
+    my $data;
+    my $curl = new WWW::Curl::Easy;
+    $curl->setopt( CURLOPT_URL,
+                   "ftp://tgftp.nws.noaa.gov/data/observations/metar/stations/$icao.TXT" );
+    $curl->setopt( CURLOPT_WRITEDATA, \$data);
+    my $retcode = $curl->perform;
+    return "curl returned $retcode (" . $curl->strerror($retcode) . ")"
+        unless ($retcode == 0);
     $data =~ s/\d+\/\d+\/\d+ \d+:\d+//;
     $data =~ s/\R//g;
     return $data;
@@ -40,15 +42,21 @@ sub weather {
     my $city = shift;
     $city =~ s/\s/+/g;
     $city = uri_escape( $city, "^A-Za-z0-9\-\._~+" );
-    my $response = $ua->get("http://www.google.com/ig/api?weather=$city");
-    my $data     = $response->decoded_content;
+    my $data;
+    my $curl = new WWW::Curl::Easy;
+    $curl->setopt( CURLOPT_URL,
+                   "http://www.google.com/ig/api?weather=$city" );
+    $curl->setopt( CURLOPT_WRITEDATA, \$data);
+    my $retcode = $curl->perform;
+    return "curl returned $retcode (" . $curl->strerror($retcode) . ")"
+        unless ($retcode == 0);
     my $xmldata  = XMLin( "$data", ValueAttr => ['data'] );
     $city = $xmldata->{'weather'}{'forecast_information'}{'city'};
     my $conditions = $xmldata->{'weather'}{'current_conditions'};
     my @current    = values %{$conditions};
     return
       "$city: $current[5] $current[2]°C/$current[1]°F $current[3] $current[4]"
-      if (@current);
+    if (@current);
 }
 
 unless ( $options{debug} ) {
@@ -67,7 +75,7 @@ until ( $SIG{INT} ) {
     die unless ($read);
     if (/PRIVMSG (\S+)/) {
         my $channel = $1;
-        if (/,me(tar)? (.+)/) {
+        if (/,me(tar)? (\w+)/) {
             syswrite( $socket, "PRIVMSG $channel :" . &metar($2) . "\n" );
         }
         elsif (/,we(ather)? (.+)/) {
